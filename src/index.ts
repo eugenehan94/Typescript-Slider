b1cb552983a543e25746196cc338af357358e143
eugenehan94 <eugene.han94@gmail.com>
2022-07-17 13:40:04 -0400

    added HTML CSS and Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,48 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const slides = document.querySelectorAll<HTMLElement>(".slide");
+const nextBtn = document.querySelector<HTMLButtonElement>(".nextBtn");
+const prevBtn = document.querySelector<HTMLButtonElement>(".prevBtn");
+slides.forEach((slide: HTMLElement, index: number) => {
+  slide.style.left = `${index * 100}%`;
+});
+let counter = 0;
+nextBtn?.addEventListener("click", () => {
+  counter++;
+  carousel();
+});
 
-import confetti from 'canvas-confetti';
+prevBtn?.addEventListener("click", ()=> {
+  counter--;
+  carousel();
+});
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+function carousel(): void {
+  // working with slides
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // }
+  // if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+  // working with buttons
+
+  if (counter < slides.length - 1) {
+    if(nextBtn != undefined)
+    nextBtn.style.display = "block";
+  } else {
+    if(nextBtn != undefined)
+    nextBtn.style.display = "none";
+  }
+  if (counter > 0) {
+    if (prevBtn != undefined)
+    prevBtn.style.display = "block";
+  } else {
+    if (prevBtn != undefined)
+    prevBtn.style.display = "none";
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
+if (prevBtn != undefined)
+prevBtn.style.display = "none";
+export {}
